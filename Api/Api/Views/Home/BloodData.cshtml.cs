3b336461b3e38e74b0d449972c42bf92c0ab552d
misteryfors <47424667+misteryfors@users.noreply.github.com>
2023-04-27 17:28:04 +0300

    Add files via upload

---
+++ Api/Api/Views/Home/BloodData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Api.Views.Home
+{
+    public class BloodDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
